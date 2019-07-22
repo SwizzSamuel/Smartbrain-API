bb36cce3f15deb9e726bbd6cca51715473b07e5c
SwizzSamuel <samuelgift09@gmail.com>
2019-07-22 18:17:20 +0100

    Smartbrain API

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'fc71831381b44fdb9f5f5f6c7ce09a7c'
+    apiKey: process.env.API_CLARIFAI
   });
 
 const handleApiCall = (req, res) => {
